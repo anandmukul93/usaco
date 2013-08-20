38fb082d3b208d26e2f32a969085240814c1722a
Mukul Anand <anandmukul93@gmail.com>
2013-08-21 01:43:44 +0530

    test.cpp added usaco 1st code

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<algorithm>
+typedef long long int ll;
+int main()
+{
+	FILE* fp1=fopen ("test.in","r");
+	FILE* fp2=fclose("test.out","w");
+	ll x,y;	
+	fscanf(fp1,"%lld %lld" &x, &y);
+	fscanf(fp2,"%lld\n",x+y);
+return 0;
+}
